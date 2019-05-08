2a9c2a238a0db66b67e8a044b6d4b72a2c3a6848
dongle <dongle74@qq.com>
2019-05-08 11:30:31 +0800

    dongle-adx  初始化

---
+++ dongle-adx/src/main/java/com/dongle/ads/adx/DongleAdxApplication.java
@@ -0,0 +1,13 @@
+package com.dongle.ads.adx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongleAdxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongleAdxApplication.class, args);
+    }
+
+}
